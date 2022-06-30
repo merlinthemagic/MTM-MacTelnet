9d1e6b8d7d6a8f30be3a931f69a873b7f5f663d1
merlinthemagic <github@martinpetermadsen.com>
2022-06-30 13:03:00 +0100

    qwd

--- Src/Tools/Shells/RouterOs/PasswordAuthentication.php
+++ Src/Tools/Shells/RouterOs/PasswordAuthentication.php
@@ -32,6 +32,7 @@ class PasswordAuthentication extends \MTM\MacTelnet\Tools\Shells\Base
 		$regExs	= array(
 				"new password\>"									=> "routeros",
 				"\[".$rawUser."\@(.+?)\] \>(\s+)?$"					=> "routeros",
+				"Do you want to see the software license\?"			=> "routeros",
 				"Login failed, incorrect username or password"		=> "error",
 				"Welcome back!"										=> "timeout" //timeout
 		);
@@ -52,13 +53,26 @@ class PasswordAuthentication extends \MTM\MacTelnet\Tools\Shells\Base
 		}
 		
 		if ($rType == "routeros") {
-			if ($rValue == "Do you want to see the software license") {
+			if ($rValue == "Do you want to see the software license\?") {
 				//we are the only ones with the information needed to clear the prompt
 				//if we dont clear it here the Destination function will have a hell of a time figuring out whats going on
 				$strCmd	= "n";
 				$regEx	= "(" . implode("|", array_keys($regExs)) . ")";
-				$ctrlObj->getCmd($strCmd, $regEx, $timeout)->get();
-			} elseif ($rValue == "new password\>") {
+				$cmdObj		= $ctrlObj->getCmd($strCmd, $regEx, $timeout);
+				$cmdObj->get();
+				$data		= $cmdObj->getReturnData(); //need return data so the prompt is not stripped out
+				
+				$rType	= null;
+				foreach ($regExs as $regEx => $type) {
+					if (preg_match("/".$regEx."/", $data) == 1) {
+						$rValue	= $regEx;
+						$rType	= $type;
+						break;
+					}
+				}
+			}
+			//there can be both a license and a forced password change one after the other
+			if ($rValue == "new password\>") {
 				//MT forcing password change, deny the change
 				$strCmd		= chr(3);
 				$regEx	= "(" . implode("|", array_keys($regExs)) . ")";
